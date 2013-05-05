f4a7ef7315d82792061a1378af8d17fa867f199e
Bilal Quadri <bilalquadri92@gmail.com>
2013-05-05 18:56:46 -0400

    ResultTableModel created

---
+++ src/edu/rutgers/cs541/gui/ResultTableModel.java
@@ -0,0 +1,43 @@
+package edu.rutgers.cs541.gui;
+
+import java.util.ArrayList;
+
+import javax.swing.table.AbstractTableModel;
+
+public class ResultTableModel extends AbstractTableModel {
+	private final ArrayList<String[]> data;
+	private final String[] columnNames = {
+		"id",
+		"Time Elapsed",
+		"# Tested",
+		"Tuple Count"
+	};
+
+	public ResultTableModel(ArrayList<String[]> data) {
+		this.data = data;
+	}
+
+	@Override
+	public int getColumnCount() {
+		return this.columnNames.length;
+	}
+
+	@Override
+	public int getRowCount() {
+		return this.data.size();
+	}
+
+	@Override
+	public Object getValueAt(int row, int col) {
+		return this.data.get(row)[col];
+	}
+
+	public void addResult(String[] result) {
+		this.data.add(result);
+		this.fireTableDataChanged();
+	}
+
+	public void getResult(int row) {
+		return this.data.get(row);
+	}
+}
