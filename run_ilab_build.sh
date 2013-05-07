0c9a54dc2233cac26242e07852db243d27ec9c98
Bilal Quadri <bilalquadri92@gmail.com>
2013-05-06 23:09:32 -0400

    Build files

---
+++ run_ilab_build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+JAVA_HOME=/usr/local/java-archive/jdk1.7.0_11/
+PATH=$JAVA_HOME/bin:/bin:/usr/bin
+
+echo 
+echo "running ant to compile code..."
+ant
+
+if [ $? != 0 ]; then
+  exit 1
+fi
+
+
